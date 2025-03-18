59dc8ddb7fae967968d7581215d89513513703dc
Joana da Matta <108022074+jodamatta@users.noreply.github.com>
2025-03-18 16:56:05 -0300

    update to use SLOS API

--- app.R
+++ app.R
@@ -1,140 +1,10 @@
 library(shiny)
 library(shinydashboard)
 library(shinyWidgets)
-library(MLmetrics)
-library(ranger)
-library(caretEnsemble)
 library(httr)
-library(magrittr)
+library(httr2)
 
-load_SLOSModel <- function() {
-  
-  old <- options()
-  on.exit(options(old))
-  
-  options(timeout = 6000)
-  url <- "https://github.com/igor-peres/ICU-Length-of-Stay-Prediction/releases/download/v2.0.0/SLOS_small.RData"
-  temp_file <- tempfile(fileext = ".RData")
-  response <- GET(url, write_disk(temp_file, overwrite = TRUE))
-  
-  if (http_error(response)) {
-    stop("Failed to download the model. HTTP error: ", status_code(response))
-  }
-  
-  load(temp_file)
-  return(small_model)
-}
-
-predict_and_evaluate <- function(data) {
-  small_model <- load_SLOSModel()
-  
-  predictions <- predict(small_model, newdata = data)
-  
-  Observations <- data.frame(Observations = data$UnitLengthStay_trunc)
-  Predictions <- data.frame(pred = predictions)
-  
-  RMSE_value <- MLmetrics::RMSE(Predictions$pred, Observations$Observations)
-  MAE_value <- MLmetrics::MAE(Predictions$pred, Observations$Observations)
-  R2_value <- MLmetrics::R2_Score(Predictions$pred, Observations$Observations)
-  comparison <- cbind(Observations, Predictions)
-  
-  return(list(predictions = data.frame(predictions), comparison = data.frame(comparison), 
-              RMSE = RMSE_value, MAE = MAE_value, R2 = R2_value))
-}
-
-SLOS <- function(data) {
-  # errors 
-  required_columns <- c("UnitCode", "UnitLengthStay_trunc")
-  missing_columns <- setdiff(required_columns, colnames(data))
-  
-  if (nrow(data) == 0) {
-    stop("Error: Input data must contain at least one row.")
-  }
-  
-  if (length(missing_columns) > 0) {
-    stop("Error: Missing required columns: ", paste(missing_columns, collapse = ", "))
-  }
-  
-  # SLOS function
-  eval_results <- predict_and_evaluate(data)
-  observations <- eval_results$comparison$Observations
-  predictions <- eval_results$predictions$pred
-  
-  df_model_pred <- data.frame(observations = observations, predictions = predictions) %>%
-    dplyr::mutate(predictions = dplyr::case_when(
-      predictions < 0 ~ 0,
-      predictions > 21 ~ 21,
-      TRUE ~ predictions
-    ))
-  
-  df_unit_slos <- df_model_pred %>%
-    dplyr::bind_cols(dplyr::select(data, UnitCode)) %>%
-    dplyr::group_by(UnitCode) %>%
-    dplyr::summarise(admissoes = dplyr::n(),
-                     soma_los_obs = sum(observations),
-                     soma_los_esp = sum(predictions)) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(SLOS = soma_los_obs / soma_los_esp) %>%
-    stats::na.omit()
-  
-  plot_SLOS_obs_prev <- ggplot2::ggplot(df_unit_slos) +
-    ggplot2::geom_point(ggplot2::aes(x = soma_los_esp, y = soma_los_obs), color = "gray40") +
-    ggplot2::geom_smooth(ggplot2::aes(x = soma_los_esp, y = soma_los_obs)) +
-    ggplot2::geom_abline(ggplot2::aes(intercept = 0, slope = 1), linetype = "dashed") +
-    ggplot2::labs(x = "Sum of predicted ICU LoS",
-                  y = "Sum of observed ICU LoS",
-                  title = "Grouped LoS per Unit (days)") +
-    ggplot2::theme_bw()
-  
-  x_range <- range(df_unit_slos$admissoes, na.rm = TRUE)
-  x_padding <- diff(x_range) * 0.1 
-  xlim <- c(max(0, x_range[1] - x_padding), x_range[2] + x_padding)
-  
-  y_range <- range(df_unit_slos$SLOS, na.rm = TRUE)
-  y_padding <- diff(y_range) * 0.1 
-  ylim <- c(max(0, y_range[1] - y_padding), y_range[2] + y_padding)
-  
-  # Compute R^2 value
-  r_squared <- cor(df_unit_slos$soma_los_esp, df_unit_slos$soma_los_obs)^2
-  
-  # Compute median and interquartile range of SLOS
-  slos_summary <- df_unit_slos %>%
-    dplyr::summarise(
-      Q1 = quantile(SLOS, 0.25, na.rm = TRUE),
-      Q3 = quantile(SLOS, 0.75, na.rm = TRUE)
-    ) %>%
-    as.list()
-  
-  slos_summary$IQR <- slos_summary$Q3 - slos_summary$Q1
-  
-  # Count units within Q1-Q3 range
-  units_within_IQR <- df_unit_slos %>%
-    dplyr::filter(SLOS >= slos_summary$Q1, SLOS <= slos_summary$Q3) %>%
-    dplyr::count()
-  
-  theta = sum(df_unit_slos$soma_los_obs) / sum(df_unit_slos$soma_los_esp)
-  
-  # Using ems::funnel to generate funnel plot
-  funnel_plot <- ems::funnel(unit = df_unit_slos$UnitCode,
-                             y = df_unit_slos$SLOS,
-                             y.type = "SRU",
-                             o = df_unit_slos$soma_los_obs,
-                             e = df_unit_slos$soma_los_esp,
-                             theta = sum(df_unit_slos$soma_los_obs) / sum(df_unit_slos$soma_los_esp),
-                             n = df_unit_slos$admissoes,
-                             method = "normal", option = "rate", plot = FALSE, direct = TRUE)
-  
-  plot(funnel_plot)
-  
-  return(list(df_unit_slos = df_unit_slos, 
-              plot_SLOS_obs_prev = plot_SLOS_obs_prev, 
-              funnel_plot = funnel_plot,
-              r_squared = r_squared,
-              slos_summary = slos_summary,
-              units_within_IQR = units_within_IQR,
-              theta = theta))
-}
-
+url <- "https://slos-api-48722054238.us-central1.run.app"
 
 ui <- dashboardPage(
   dashboardHeader(title = "ICU Analysis"),
@@ -210,7 +80,7 @@ ui <- dashboardPage(
                     pickerInput("ChfNyha", "Cardiac Heart Failure:", choices = c("Class23", "Class4", "No"), selected = "No"),
                     numericInput("lowest_gcs", "Lowest Glasgow Coma Scale (3-15):", value = 10, min = 3, max = 15)
                 ),
-              
+                
                 box(title = "Clinical Measurements", status = "warning", solidHeader = TRUE, width = 4,
                     numericInput("urea", "Urea (first 1h):", value = 20, min = 0),
                     numericInput("length_hospital_stay", "Length of Hospital Stay Prior to ICU (days):", value = 5, min = 0),
@@ -246,7 +116,7 @@ ui <- dashboardPage(
                 box(width = 4, 
                     textOutput("predict_status"),
                     actionButton("predict", "Predict", icon = icon("calculator"), class = "btn-success")
-                    ),
+                ),
                 box(title = "Predicted Length of Stay:", status = "success", solidHeader = TRUE, width = 4,
                     verbatimTextOutput("prediction"))
               )
@@ -286,13 +156,34 @@ server <- function(input, output) {
   
   result_metrics_reactive <- reactive({
     req(data_reactive())  
-    status$eval <- ""  
-    result <- SLOS(data_reactive())  
+    status$eval <- "" 
+    
+    #########################
+    #       SLOS TEST       #
+    #########################
+    # result <- SLOS(data_reactive()) 
+    temp_file <- tempfile(fileext =".rds")
+    saveRDS(data_reactive(), temp_file)
+    url_SLOS <- paste(url, "/SLOS_API", sep ="")
+    
+    response <- request(url_SLOS) %>%
+      req_body_multipart(file = upload_file(temp_file)) %>%
+      req_timeout(200) %>%
+      req_perform()
+    
+    temp_rds_file <- tempfile(fileext = ".rds")
+    
+    writeBin(resp_body_raw(response), temp_rds_file)
+    
+    result <- readRDS(temp_rds_file)
+    
+    unlink(temp_rds_file)
+    
     return(result)  
   })
   
   output$funnel_plot <- renderPlot({
-    req(result_metrics_reactive())  # 
+    req(result_metrics_reactive())  
     plot(result_metrics_reactive()$funnel_plot)  
   })
   
@@ -396,8 +287,26 @@ server <- function(input, output) {
       IsCirrhosis = factor(input$cirrhosis),
       IsCrf = factor(input$crf)
     )
+    #######################
+    #   TEST PREDICTION   #
+    #######################
+    # predicted_los <- predict_and_evaluate(predictors)
+    url_pne <- paste(url, "/predict_and_evaluate_API", sep="")
     
-    predicted_los <- predict_and_evaluate(predictors)
+    temp_file <- tempfile(fileext = ".rds")
+    saveRDS(predictors, temp_file)
+    
+    response <- request("https://slos-api-48722054238.us-central1.run.app/predict_and_evaluate_API") %>%
+      req_body_multipart(file = upload_file(temp_file)) %>%
+      req_timeout(200) %>%
+      req_perform()
+    
+    temp_rds_file <- tempfile(fileext = ".rds")
+    
+    writeBin(resp_body_raw(response), temp_rds_file)
+    
+    predicted_los <- readRDS(temp_rds_file)
+    unlink(temp_file)
     
     output$prediction <- renderText({
       paste0(round(predicted_los$predictions, 2), " days")
