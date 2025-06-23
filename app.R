91e5db02d6edd4eafe41419cce05265362771523
Joana da Matta <108022074+jodamatta@users.noreply.github.com>
2025-06-23 13:41:24 -0300

    update shiny dashboard

--- app.R
+++ app.R
@@ -54,8 +54,22 @@ ui <- dashboardPage(
                       
                       downloadButton("download_rdata", "Download Predictions (RData)"),
                       downloadButton("download_csv", "Download Predictions (CSV)")
-                    )
+                    ),
+                    
                 ),
+                
+                box(
+                  title = "Documentation",
+                  status = "info",
+                  solidHeader = TRUE,
+                  width = 6,
+                  tags$p(
+                    "For details on the data format, see the ",
+                    tags$a(href = "https://github.com/igor-peres/ICU-Length-of-Stay-Prediction/raw/main/DataDictionary.pdf",
+                           "Data Dictionary (PDF)", target = "_blank")
+                  )
+                ),
+                
                 
                 
                 box(title = "Analysis Plots", status = "info", solidHeader = TRUE, width = 9,
@@ -69,57 +83,84 @@ ui <- dashboardPage(
               fluidRow(
                 box(title = "Patient Information", status = "primary", solidHeader = TRUE, width = 3,
                     pickerInput("gender", "Gender:", choices = c("Female", "Male"), selected = "Female"),
-                    numericInput("age", "Age (>= 16):", value = 30, min = 16),
+                    numericInput("age", "Age (>= 16):", value = 22, min = 16),
                     pickerInput("admission_source", "Source of ICU Admission:", 
                                 choices = c("Cardiovascular intervention room", "Emergency room", 
                                             "Operating room", "Other", "Other unit at your hospital", 
-                                            "Ward/Floor"), selected = "Other"),
+                                            "Ward/Floor"), selected = "Emergency room"),
                     pickerInput("admission_type", "Type of ICU Admission:", 
-                                choices = c("Medical", "Emergency Surgery", "Scheduled Surgery")),
-                    pickerInput("cirrhosis", "Cirrhosis:", choices = c("ChildAB", "ChildC", "No"), selected = "No"),
-                    pickerInput("crf", "Chronic Renal Failure:", choices = c("Dialysis", "NoDialysis", "No"), selected = "No"),
-                    pickerInput("ChfNyha", "Cardiac Heart Failure:", choices = c("Class23", "Class4", "No"), selected = "No"),
-                    numericInput("lowest_gcs", "Lowest Glasgow Coma Scale (3-15):", value = 10, min = 3, max = 15)
+                                choices = c("Medical", "Emergency Surgery", "Scheduled Surgery"), selected = "Medical")
                 ),
                 
                 box(title = "Clinical Measurements", status = "warning", solidHeader = TRUE, width = 4,
-                    numericInput("urea", "Urea (first 1h):", value = 20, min = 0),
-                    numericInput("length_hospital_stay", "Length of Hospital Stay Prior to ICU (days):", value = 5, min = 0),
-                    numericInput("creatinine", "Highest Creatinine (first 1h)", value = 0, min = 0),
-                    numericInput("high_heart", "Highest Heart Rate (first 1h):", value = 0, min = 0, max = 200),
-                    numericInput("high_temp", "Highest temperature (first 1h):", value = 0, min = 0, max = 50),
-                    numericInput("high_leuko", "Highest Leukocyte count (first 1h):", value = 0, min = 0),
-                    numericInput("high_resp", "Highest Respiratory Rate (first 1h):", value = 0, min = 0)
+                    numericInput("urea", "Urea (first 1h):", value = 37, min = 0),
+                    numericInput("length_hospital_stay", "Length of Hospital Stay Prior to ICU (days):", value = 0, min = 0),
+                    numericInput("creatinine", "Highest Creatinine (first 1h)", value = 0.9, min = 0),
+                    numericInput("high_heart", "Highest Heart Rate (first 1h):", value = 79, min = 0, max = 200),
+                    numericInput("high_temp", "Highest temperature (first 1h):", value = 36.1, min = 0, max = 50),
+                    numericInput("high_leuko", "Highest Leukocyte count (first 1h):", value = 8.9, min = 0),
+                    numericInput("high_resp", "Highest Respiratory Rate (first 1h):", value = 18, min = 0),
+                    numericInput("lowest_gcs", "Lowest Glasgow Coma Scale (3-15):", value = 15, min = 3, max = 15)
                 ),
                 
-                box(title = "Comorbidities", status = "danger", solidHeader = TRUE, width = 5,
-                    checkboxInput("dementia", "Dementia", FALSE),
-                    checkboxInput("aids", "AIDS", FALSE),
-                    checkboxInput("alcohol", "Alcoholism", FALSE),
-                    checkboxInput("hypertension", "Arterial Hypertension", FALSE),
-                    checkboxInput("asthma", "Asthma", FALSE),
-                    checkboxInput("arrhythmia", "Cardiac Arrhythmia", FALSE),
-                    checkboxInput("aaf", "Acute Atrial Fibrillation", FALSE),
-                    checkboxInput("aki", "Acute Kidney Injury", FALSE),
-                    checkboxInput("angina", "Angina", FALSE),
-                    checkboxInput("asystole", "Asystole", FALSE),
-                    checkboxInput("transplant", "Cardiac Transplant", FALSE),
-                    checkboxInput("chemo", "Chemotherapy", FALSE),
-                    checkboxInput("caf", "Chronic Atrial Fibrillation", FALSE),
-                    checkboxInput("ventilation", "Non-invasive Ventilation at admission (first 24h)", FALSE),
-                    checkboxInput("resp_failure", "Respiratory Failure at admission (first 24h)", FALSE),
+                box(title = "Use of Devices (first 24h)", status = "danger", solidHeader = TRUE, width = 4,
+                    checkboxInput("ventilation", "Non-invasive Ventilation at admission", FALSE),
+                    checkboxInput("resp_failure", "Respiratory Failure at admission", FALSE),
                     checkboxInput("is_mechanical_ventilation", "Mechanical Ventilation", FALSE),
-                    checkboxInput("is_vasopressors", "Vasopressors at admission (first 24h)", FALSE)
-                )
-              ),
-              
-              fluidRow(
-                box(width = 4, 
-                    textOutput("predict_status"),
-                    actionButton("predict", "Predict", icon = icon("calculator"), class = "btn-success")
+                    checkboxInput("is_vasopressors", "Vasopressors at admission", FALSE)
                 ),
-                box(title = "Predicted Length of Stay:", status = "success", solidHeader = TRUE, width = 4,
-                    verbatimTextOutput("prediction"))
+                
+                box(title = "Comorbidities", status = "danger", solidHeader = TRUE, width = 4,
+                    pickerInput("cirrhosis", "Cirrhosis:", choices = c("ChildAB", "ChildC", "No"), selected = "No"),
+                    pickerInput("crf", "Chronic Renal Failure:", choices = c("Dialysis", "NoDialysis", "No"), selected = "No"),
+                    pickerInput("ChfNyha", "Cardiac Heart Failure:", choices = c("Class23", "Class4", "No"), selected = "No"),
+                    fluidRow(
+                      column(6,
+                             checkboxInput("dementia", "Dementia", FALSE),
+                             checkboxInput("aids", "AIDS", FALSE),
+                             checkboxInput("alcohol", "Alcoholism", FALSE),
+                             checkboxInput("hypertension", "Arterial Hypertension", FALSE),
+                             checkboxInput("asthma", "Asthma", FALSE),
+                             checkboxInput("arrhythmia", "Cardiac Arrhythmia", FALSE)
+                      ),
+                      column(6,
+                             checkboxInput("aaf", "Acute Atrial Fibrillation", FALSE),
+                             checkboxInput("aki", "Acute Kidney Injury", FALSE),
+                             checkboxInput("angina", "Angina", FALSE),
+                             checkboxInput("asystole", "Asystole", FALSE),
+                             checkboxInput("transplant", "Cardiac Transplant", FALSE),
+                             checkboxInput("chemo", "Chemotherapy", FALSE),
+                             checkboxInput("caf", "Chronic Atrial Fibrillation", FALSE)
+                      )
+                    )
+                ),
+                box(
+                  title = "Predicted Length of Stay:",
+                  status = "success",
+                  solidHeader = TRUE,
+                  width = 6,  # Adjust width as needed
+                  fluidRow(
+                    column(width = 6,
+                           textOutput("predict_status"),
+                           actionButton("predict", "Predict", icon = icon("calculator"), class = "btn-success")
+                    ),
+                    column(width = 6,
+                           tags$h4("Result:"),
+                           verbatimTextOutput("prediction")
+                    )
+                  )
+                ),
+                box(
+                  title = "Documentation",
+                  status = "info",
+                  solidHeader = TRUE,
+                  width = 6,
+                  tags$p(
+                    "For details on the data format, see the ",
+                    tags$a(href = "https://github.com/igor-peres/ICU-Length-of-Stay-Prediction/raw/main/DataDictionary.pdf",
+                           "Data Dictionary (PDF)", target = "_blank")
+                  )
+                )
               )
       )
     )
@@ -263,29 +304,29 @@ server <- function(input, output) {
       LowestGlasgowComaScale1h = input$lowest_gcs,
       Urea = input$urea,
       LengthHospitalStayPriorUnitAdmission = input$length_hospital_stay,
-      IsMechanicalVentilation = factor(as.integer(input$is_mechanical_ventilation), levels = c(1, 0)),
-      IsVasopressors = factor(as.integer(input$is_vasopressors), levels = c(1, 0)),
+      IsMechanicalVentilation = factor(as.integer(input$is_mechanical_ventilation), levels = c(0, 1)),
+      IsVasopressors = factor(as.integer(input$is_vasopressors), levels = c(0, 1)),
       HighestCreatinine1h = as.numeric(input$creatinine),
-      IsNonInvasiveVentilation = factor(as.integer(input$ventilation), levels = c(1, 0)),
-      IsRespiratoryFailure = factor(as.integer(input$resp_failure), levels = c(1, 0)),
-      IsDementia = factor(as.integer(input$dementia), levels = c(1, 0)),
+      IsNonInvasiveVentilation = factor(as.integer(input$ventilation), levels = c(0, 1)),
+      IsRespiratoryFailure = factor(as.integer(input$resp_failure), levels = c(0, 1)),
+      IsDementia = factor(as.integer(input$dementia), levels = c(0, 1)),
       HighestHeartRate1h = as.numeric(input$high_heart),
       HighestTemperature1h = as.numeric(input$high_temp),
       ChfNyha = factor(input$ChfNyha),
       HighestLeukocyteCount1h = as.numeric(input$high_leuko),
       HighestRespiratoryRate1h = as.numeric(input$high_resp),
-      IsAcuteAtrialFibrilation = factor(as.integer(input$aaf), levels = c(1, 0)),
-      IsAcuteKidneyInjury = factor(as.integer(input$aki), levels = c(1, 0)),
-      IsAids = factor(as.integer(input$aids), levels = c(1, 0)),
-      IsAlcoholism = factor(as.integer(input$alcohol), levels = c(1, 0)),
-      IsAngina = factor(as.integer(input$angina), levels = c(1, 0)),
-      IsArterialHypertension = factor(as.integer(input$hypertension), levels = c(1, 0)),
-      IsAsthma = factor(as.integer(input$asthma), levels = c(1, 0)),
-      IsAsystole = factor(as.integer(input$asystole), levels = c(1, 0)),
-      IsCardiacArrhythmia = factor(as.integer(input$arrhythmia), levels = c(1, 0)),
-      IsCardiacTransplant = factor(as.integer(input$transplant), levels = c(1, 0)),
-      IsChemotherapy = factor(as.integer(input$chemo), levels = c(1, 0)),
-      IsChronicAtrialFibrilation = factor(as.integer(input$caf), levels = c(1, 0)),
+      IsAcuteAtrialFibrilation = factor(as.integer(input$aaf), levels = c(0, 1)),
+      IsAcuteKidneyInjury = factor(as.integer(input$aki), levels = c(0, 1)),
+      IsAids = factor(as.integer(input$aids), levels = c(0, 1)),
+      IsAlcoholism = factor(as.integer(input$alcohol), levels = c(0, 1)),
+      IsAngina = factor(as.integer(input$angina), levels = c(0, 1)),
+      IsArterialHypertension = factor(as.integer(input$hypertension), levels = c(0, 1)),
+      IsAsthma = factor(as.integer(input$asthma), levels = c(0, 1)),
+      IsAsystole = factor(as.integer(input$asystole), levels = c(0, 1)),
+      IsCardiacArrhythmia = factor(as.integer(input$arrhythmia), levels = c(0, 1)),
+      IsCardiacTransplant = factor(as.integer(input$transplant), levels = c(0, 1)),
+      IsChemotherapy = factor(as.integer(input$chemo), levels = c(0, 1)),
+      IsChronicAtrialFibrilation = factor(as.integer(input$caf), levels = c(0, 1)),
       IsCirrhosis = factor(input$cirrhosis),
       IsCrf = factor(input$crf)
     )
